7c38277ea21ff9bf746f015369737de89df453c3
madhav9691 <madhavaraochennapragada@gmail.com>
2020-05-12 13:30:57 +0530

    Initial commit

---
+++ public/javascripts/birthday.js
@@ -0,0 +1,34 @@
+ var app=angular.module('birthdayApp',[]);
+app.controller('MyCtrl',['$scope', '$http',function($scope,$http) {
+
+
+	$http({
+		method:'GET',
+		url:'/getdetailes'
+	}).then(function success(response){
+		// console.log(req.body);
+		console.log(response.data);
+		// alert("retrive success");
+		$scope.birthdays=response.data;
+	},function error(response){
+		console.log(response.data)
+		alert('faild to retrivedata');
+	})
+
+	$scope.sendmail=function(birth){
+		console.log($scope.birth);
+		$http({
+			method:"POST",
+			url:'/postbirthday',
+			data:$scope.birth
+		}).then(function success(response){
+			alert('inserted succesfully');
+
+			$scope.birth={}
+
+		},function error(response){
+			alert('faild to sent wishes');
+		});
+	}
+
+}])
---
+++ routes/index.js
@@ -0,0 +1,47 @@
+ var express = require('express');
+ var router = express.Router();
+ var monk =require('monk');
+ var moment=require('moment');
+ var database=monk('localhost:27017/birthday')
+ var colec=database.get('birthdays')
+
+ /* GET home page. */
+ router.get('/', function(req, res) {
+ 	res.render('index');
+ });
+
+ router.post('/postbirthday',function(req,res){
+ 	console.log(req.body);
+ 	var Data={
+ 		Name:req.body.name,
+ 		Email:req.body.email,
+ 		DOB:moment(req.body.date).format('DD-MM-YYYY'),
+ 		Mobile:req.body.number
+ 	}
+ 	colec.insert(Data,function(err,docs){
+ 		if(err){
+ 			console.log(err);
+ 		}
+ 		else{
+ 			console.log(docs);
+ 			res.send(docs);
+ 		}
+ 	})
+ 	
+ })
+ /*retrive data*/
+ router.get('/getdetailes',function(req,res){
+
+ 	colec.find({},function(err,docs){
+
+ 		if(err){
+ 			console.log("err");
+
+ 		}
+ 		else{
+ 			console.log("docs");
+ 			res.send(docs);
+ 		}
+ 	})
+ })
+ module.exports = router;
